84f86c1b8fe97a0444d0d7941e6f95a34082073c
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-07-23 12:03:20 -0400

    configs

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const users = ["josh"];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.send(users);
 });
 
 module.exports = router;
