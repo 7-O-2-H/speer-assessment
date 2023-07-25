1f3b9fe5e07b5baefdef4dcd7514c95091317180
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-07-25 12:11:20 -0400

    bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,26 +2,18 @@ const express = require('express');
 const router = express.Router();
 const authQueries = require('../db/queries/auth');
 const cookieSession = require("cookie-session");
-app.use(cookieSession({
-  name: 'session',
-  keys: ["key1", "key2"],
-
-  maxAge: 24 * 60 * 60 * 1000
-}));
 
 //bcrypt
 
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 /* GET auth  */
-router.get('/auth', function(req, res) {
-  authQueries.getAllUsers()
-  .then(data => {
-    res.json(data);
-  })
-});
 
 router.get('/auth/login', function(req, res) {
+  const user_id = req.session.user_id;
+  if (user_ud) {
+    res.send("You are already logged in.")
+  }
 });
 
 router.post('/auth/login', function(req, res) {
@@ -33,44 +25,46 @@ router.post('/auth/login', function(req, res) {
   authQueries.getUserByuser(user)
   .then(data => {
     if (!data[0]) {
-      return res.json([false, `Error: user not in database: ${user}`]);
+      return res.send([false, `Error: user not in database: ${user}`]);
     }
 
-    if (password != data[0].password) {
-      return res.json([false, 'Error: Your password is incorrect!']);
+    if (hashedPassword != data[0].password) {
+      return res.send([false, 'Error: Your password is incorrect!']);
     }
 
     //set user
     req.session.user_id = data[0].id;
     return res.json([true, req.session.user_id]);
   });
-
  
 });
 
 router.get('/auth/signup', function(req, res) {
-  console.log('hello');
+  
 })
 
 router.post('/auth/signup', function(req, res) {
 
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
   newUser = {
     name: req.body.name,
     user: req.body.user,
-    password: req.body.password
+    password: hashedPassword
   }
 
+  
   //Check if user already exists
   authQueries.getUserByuser(newUser.user).then(data => {
     if (data[0]) {
-      return res.json([false, 'Error: this e-mail already in our database.']);
+      return res.send([false, 'Error: this e-mail already in our database.']);
     };
   });
 
   //Add new user
   authQueries.addUser(newUser).then(data => {
     req.session.user_id = data[0].id;
-    return res.json([true, req.session.user_id]);
+    return res.send([true, req.session.user_id]);
   })
 
 });
